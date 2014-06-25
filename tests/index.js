ea86b7092a6595bd70d76a78011de95514d8177c
Jacek Dunikowski <jacekdunikowski@wp.pl>
2014-06-25 23:20:57 +0200

    tests

---
+++ tests/index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var assert = require('assert');
+
+suite('submitAnswers', function() {
+
+  test('server initialization', function(done, server) {
+    server.eval(function() {
+      var collection = POSTS.find().fetch();
+      emit('collection', collection);
+    }).once('collection', function(collection) {
+      assert.equal(collection.length, 0);
+      done();
+    });
+  });
+
+  test('server insert : OK', function(done, server, client) {
+    server.eval(function() {
+      Posts.insert({commenttext: "testss"  });
+      var collection = Posts.find().fetch();
+      emit('collection', collection);
+    }).once('collection', function(collection) {
+      assert.equal(collection.length, 1);
+      done();
+    });
+
+    client.once('collection', function(collection) {
+      assert.equal(Posts.find().fetch().length, 1);
+      done();
+    });
+  });
+
+});
