88800d38ff2b12fec7e94ac3409847de99854343
Jacek Dunikowski <jacekdunikowski@wp.pl>
2014-06-26 00:12:36 +0200

    testy travis

--- tests/index.js
+++ tests/index.js
@@ -25,12 +25,17 @@ suite('submitAnswers', function() {
       done();
     });
 
-    test('server update : OK', function(done, server, client) {
+
+
+    client.once('collection', function(collection) {
+      assert.equal(Posts.find().fetch().length, 1);
+      done();
+    });
+  });
+
+  test('server insert : OK', function(done, server, client) {
     server.eval(function() {
-      Posts.update(answerID,{
-        $inc : {'yes':1},
-        $set: {'votedBy': Meteor.userId()}
-      });
+      Posts.insert({commenttext: "testss"  });
       var collection = Posts.find().fetch();
       emit('collection', collection);
     }).once('collection', function(collection) {
@@ -38,10 +43,35 @@ suite('submitAnswers', function() {
       done();
     });
 
+    
+
     client.once('collection', function(collection) {
       assert.equal(Posts.find().fetch().length, 1);
       done();
     });
   });
+
+  test('server update : OK', function(done, server, client) {
+    server.eval(function() {
+     Posts.update('1',{
+      $inc : {'yes':1},
+      $set: {'votedBy': '21sa124dsavr'}
+    });
+      var collection = Posts.find().fetch();
+      emit('collection', collection);
+    }).once('collection', function(collection) {
+      assert.equal(collection.length, 1);
+      done();
+    });
+
+    
+
+    client.once('collection', function(collection) {
+      assert.equal(Posts.find().fetch().length, 1);
+      done();
+    });
+  });
+
+  
 
 });
