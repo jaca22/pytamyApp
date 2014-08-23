d2f41108b8239863510c4088f04378c8aa457e6e
Jacek Dunikowski <jacekdunikowski@wp.pl>
2014-08-23 22:55:49 +0200

    test

--- tests/index.js
+++ tests/index.js
@@ -33,7 +33,7 @@ suite('Meteor App tests', function() {
     });
   });
 
-  
+
 
   test('server insert : OK', function(done, server, client) {
     server.eval(function() {
@@ -53,30 +53,44 @@ suite('Meteor App tests', function() {
     });
   });
 
-ltest('access granted for loggedin users', function(done, server, client) {
-server.eval(function() {
-Accounts.createUser({email: 'a@a.com', password: '123456'});
-emit('done');
-}).once('done', function() {
-server.eval(observeCollection);
-});
-function observeCollection() {
-Posts.find().observe({
-added: function(doc) {
-emit('added', doc);
-}
-});
-}
-server.once('added', function(doc) {
-assert.equal(doc.title, 'hello');
-done();
-});
-client.eval(function() {
-Meteor.loginWithPassword('a@a.com', '123456', function() {
-Posts.insert({title: 'hello'});
-});
-});
-});
+  test('access granted for loggedin users', function(done, server, client) {
+    server.eval(function() {
+      Accounts.createUser({email: 'a@a.com', password: '123456'});
+      emit('done');
+    }).once('done', function() {
+      server.eval(observeCollection);
+    });
+    function observeCollection() {
+      Posts.find().observe({
+        added: function(doc) {
+          emit('added', doc);
+        }
+      });
+    }
+    server.once('added', function(doc) {
+      assert.equal(doc.title, 'hello');
+      done();
+    });
+    client.eval(function() {
+      Meteor.loginWithPassword('a@a.com', '123456', function() {
+        Posts.insert({commenttext: 'hello'});
+      });
+    });
+  });
   
+  test('access denied for normal users', function(done, server, client) {
+    client.eval(function() {
+      Posts.find().observe({
+        removed: function(doc) {
+          emit('remove', doc);
+        }
+      });
+      Posts.insert({commenttext: 'hello'});
+    });
+    client.once('remove', function(doc) {
+      assert.equal(doc.title, 'hello');
+      done();
+    });
+  });
 
 });
