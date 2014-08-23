ef3d3509a33188ec743bbf6f5ace46a98224b4f6
Jacek Dunikowski <jacekdunikowski@wp.pl>
2014-08-23 23:07:35 +0200

    test

--- tests/index.js
+++ tests/index.js
@@ -15,7 +15,7 @@ suite('Meteor App tests', function() {
   });
 
 
-  test('insert : OK', function(done, server, client) {
+  test('client insert : OK', function(done, server, client) {
     server.eval(function() {
       Posts.insert({commenttext: "tests"  });
       var collection = Posts.find().fetch();
@@ -53,44 +53,24 @@ suite('Meteor App tests', function() {
     });
   });
 
-  test('access granted for loggedin users', function(done, server, client) {
+
+ test('server remove : OK', function(done, server, client) {
     server.eval(function() {
-      Accounts.createUser({email: 'a@a.com', password: '123456'});
-      emit('done');
-    }).once('done', function() {
-      server.eval(observeCollection);
-    });
-    function observeCollection() {
-      Posts.find().observe({
-        added: function(doc) {
-          emit('added', doc);
-        }
-      });
-    }
-    server.once('added', function(doc) {
-      assert.equal(doc.title, 'hello');
+      Posts.remove({_id: 1  });
+      var collection = Posts.find().fetch();
+      emit('collection', collection);
+    }).once('collection', function(collection) {
+      assert.equal(collection.length, 1);
       done();
     });
-    client.eval(function() {
-      Meteor.loginWithPassword('a@a.com', '123456', function() {
-        Posts.insert({commenttext: 'hello'});
-      });
-    });
-  });
-  
-  test('access denied for normal users', function(done, server, client) {
-    client.eval(function() {
-      Posts.find().observe({
-        removed: function(doc) {
-          emit('remove', doc);
-        }
-      });
-      Posts.insert({commenttext: 'hello'});
-    });
-    client.once('remove', function(doc) {
-      assert.equal(doc.title, 'hello');
+
+    
+    client.once('collection', function(collection) {
+      assert.equal(Posts.find().fetch().length, 1);
       done();
     });
   });
+
+ 
 
 });
