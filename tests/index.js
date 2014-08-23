84c80f33ea3294f1425876cace27e231c9ab379b
Jacek Dunikowski <jacekdunikowski@wp.pl>
2014-08-23 23:33:50 +0200

    test

--- tests/index.js
+++ tests/index.js
@@ -71,6 +71,14 @@ suite('Meteor App tests', function() {
     });
   });
 
+test("can't create when not logged in", function(done, server, client) {
+console.log('here');
+var ret = client.evalSync(function() {
+emit('return', 'foo');
+});
+console.log(ret);
+done();
+});
  
 
 });
