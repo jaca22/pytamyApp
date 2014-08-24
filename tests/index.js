676ac4225b0b63b0632a84bb1a5f29dd0872802a
Jacek Dunikowski <jacekdunikowski@wp.pl>
2014-08-24 13:14:30 +0200

    testy

--- tests/index.js
+++ tests/index.js
@@ -93,7 +93,7 @@ suite('Meteor App tests', function() {
       Posts.insert(post);
       Posts.update(1,{
         $inc : {'yes':1},
-        $set: {'votedBy': this.userId()}
+        $set: {'votedBy': 1}
       });
       var collection = Posts.find().fetch();
       emit('collection', collection);
